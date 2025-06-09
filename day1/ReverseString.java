1eeb3c3165bb1d31513b61bbd9d62c9ecaccc06c
g-jacks <gsirijack@gmail.com>
2025-06-09 06:21:54 +0530

    Reverse a String q1

--- day1/ReverseString.java
+++ day1/ReverseString.java
@@ -1,9 +1,27 @@
 package day1;
 
+import java.util.Scanner;
+
 public class ReverseString {
+	// To reverse a String
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String st = sc.next();
+		
+		char[] arr = st.toCharArray();
+		int i=0;
+		int  j=arr.length-1;
+		while(i<j) {
+			char temp = arr[i];
+			arr[i]=arr[j];
+			arr[j]=temp;
+			i++;
+			j--;
+		}
+		String res = new String(arr);
+		
+		System.out.println(res); 
 
 	}
 
